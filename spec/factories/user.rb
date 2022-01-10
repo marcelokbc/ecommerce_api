e76e8f36eab7e30754c023164ded8cc99a7ff3e7
Marcelo Silva <marcelo.cansilva@gmail.com>
2022-01-09 21:42:56 -0300

    Created table Coupon, model and rspec

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence { Faker::Name.name}
+    name { Faker::Name.name}
     email { Faker::Internet.email}
     password { '123456'}
     password_confirmation { '123456'}
