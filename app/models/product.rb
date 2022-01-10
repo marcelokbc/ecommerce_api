1347def8aa325789bc0eb4f4f65b1b9b564b83f2
Marcelo Silva <marcelo.cansilva@gmail.com>
2022-01-09 21:16:18 -0300

    Created table ProductCategory, model and rspec

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
+  has_many :product_categories, dependent: :destroy
+	has_many :categories, through: :product_categories
   belongs_to :productable, polymorphic: true
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
