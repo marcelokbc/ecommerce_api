14b907972351f0ae8df5b27f713f878be115a072
Marcelo Silva <marcelo.cansilva@gmail.com>
2022-01-24 18:07:32 -0300

    Added migration and tests product status

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,17 @@
 class Product < ApplicationRecord
-  has_many :product_categories, dependent: :destroy
-	has_many :categories, through: :product_categories
   belongs_to :productable, polymorphic: true
-  has_one_attached :image
-
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
+
+  has_many :product_categories, dependent: :destroy
+	has_many :categories, through: :product_categories
+  
+  has_one_attached :image
   validates :image, presence: true
+  validates :status, presence: true
+
+  enum status: { available: 1, unavailable: 2 }
 
   include NameSearchable
 	include Paginatable
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   subject { build(:product) }
 
-  before {puts subject.image }
-
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   it { is_expected.to validate_presence_of(:description) }
@@ -16,6 +14,8 @@ RSpec.describe Product, type: :model do
   it { is_expected.to have_many(:categories).through(:product_categories) }
 
   it { is_expected.to validate_presence_of(:image) }
+  it { is_expected.to validate_presence_of(:status) }
+  it { is_expected.to define_enum_for(:status).with_values({ available: 1, unavailable: 2}) }
 
   it_behaves_like "name searchable concern", :product
   it_behaves_like "paginatable concern", :product
